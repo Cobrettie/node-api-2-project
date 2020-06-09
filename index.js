2cb7b507a1477a8466399729eb335c4911c0964c
Cobrettie <cobrettiegarner3@gmail.com>
2020-06-08 20:59:55 -0400

    create basic server

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
 const express = require('express');
+const cors = require('cors');
+const shortid = require('shortid')
+
 const server = express();
+
+server.use(express.json());
+server.use(cors());
+
 
 server.listen(5000)
