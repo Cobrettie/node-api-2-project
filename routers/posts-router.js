f3824a2c07f478e244f6113ab8051a6f5a4a4dea
Cobrettie <cobrettiegarner3@gmail.com>
2020-06-09 22:16:23 -0400

    build get request endpoint for /api/posts/:id/comments

--- routers/posts-router.js
+++ routers/posts-router.js
@@ -8,9 +8,53 @@ const router = express.Router();
 
 router.get('/api/posts', (req, res) => {
   posts.find(req.body)
-    .then(posts => res.status(200).json(posts))
+    .then((posts) => res.status(200).json(posts))
     .catch(err => {
-      res.status(500).json({ error: "The posts information could not be retrieved." })
+      console.log(err);
+      res.status(500).json({ 
+        error: "The posts information could not be retrieved." 
+      })
+    })
+})
+
+router.get('/api/posts/:id', (req, res) => {
+  posts.findById(req.params.id)
+    .then((post) => {
+      if (post.length === 0) {
+        res.status(404).json({ 
+          message: "The post with the specified ID does not exist." 
+        })
+      } else {
+        res.status(200).json(post)
+      }
+    })
+    .catch(err => {
+      console.log(err)
+      res.status(500).json({
+          error: "The post information could not be retrieved."
+        })
+    })
+})
+
+router.get('/api/posts/:id/comments', (req, res) => {
+  posts.findById(req.params.id)
+    .then((post) => {
+      if (post.length === 0) {
+        res.status(404).json({
+          message: "The post with the specified ID does not exist."
+        })
+      } else if (post) {
+        return posts.findPostComments(req.params.id)
+      }
+    })
+    .then((comments) => {
+      res.json(comments)
+    })
+    .catch(err => {
+      console.log(err)
+      res.status(500).json({
+        message: "The comments information could not be retrieved."
+      })
     })
 })
 
@@ -26,12 +70,15 @@ router.post('/api/posts', (req, res) => {
       res.status(201).json(post)
     })
     .catch(err => {
-      res.status(500).json({ error: "There was an error while saving the post to the database" })
+      console.log(err);
+      res.status(500).json({ 
+        error: "There was an error while saving the post to the database" 
+      })
     })
 })
 
 router.post('/api/posts/:id/comments', (req, res) => {
-  if (req) {}
+  // if (req) {}
 })
 
 module.exports = router;
--- routers/welcome-router.js
+++ routers/welcome-router.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
 
+const server = express();
+
 server.get('/', (req, res) => {
   res.status(200).json({ message: 'success brolly, server running' })
 })
